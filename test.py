181b4a09a82126daf354ebf67387c6258a73689d
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-02-25 16:58:51 +0300

    message #P1-1 Fixed Critical

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+asdaadsasdasads
 
 24 Apr 2013 15:25:17,090 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Start processing request /issue/IBN-2
 24 Apr 2013 15:25:17,090 DEBUG [BaseApplication               ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Created bean for session variables
