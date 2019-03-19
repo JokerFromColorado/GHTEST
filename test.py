8f01dfec27dd3cad0424abffa14b6a99b92a0d59
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-03-19 15:29:22 +0300

    message #NPJ-1 Fixed Critical

--- test.py
+++ test.py
@@ -7,12 +7,4 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:33,922 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Finish processing request /projects in 16ms
 24 Apr 2013 15:25:35,825 INFO  [MainServlet                   ] [http request: /editProject/IBN                    ] [georgiosd@127.0.0.1] Start processing request /editProject/IBN
 24 Apr 2013 15:25:35,872 INFO  [MainServlet                   ] [http request: /editProject/IBN                    ] [georgiosd@127.0.0.1] Finish processing request /editProject/IBN in 47ms
-2
-24 Apr 2013 15:26:48,428 INFO  [MainServlet                   ] [TeamcityBuildTypeMappingList.refreshStatuses:tick]] [georgiosd@127.0.0.1] Start processing request /_events?window_id=41 [l.E.tab_TeamCity.TeamcityBuildTypeMappingList.refreshStatuses:tick]
-24 Apr 2013 15:26:48,428 INFO  [MainServlet                   ] [TeamcityBuildTypeMappingList.refreshStatuses:tick]] [georgiosd@127.0.0.1] Finish processing request /_events?window_id=41 qweewqwweq
-rsr eqqwe 
- wqqweqwewq
- qwewqqwetatuses:tick] in 15ms
-
-asdaasd
-asdadsadsqwewqe
+test test test
