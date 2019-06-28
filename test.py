5fd2e1ff92bc3aa3880714b3cd534adbcce7d283
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-06-28 15:45:57 +0300

    fix #JT-2 fixed critical

--- test.py
+++ test.py
@@ -4,8 +4,4 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,090 DEBUG [BaseApplication               ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Created bean for session variables
 24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
 24 Apr 2013 15:25:33,906 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Start processing request /projects
-24 Apr 2013 15:25:33,922 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Finish processing request /projects in 16ms
-24 Apr 2013 15:25:35,825 INFO  [MainServlet                   ] [http request: /editProject/IBN                    ] [georgiosd@127.0.0.1] Start processing request /editProject/IBN
-24 Apr 2013 15:25:35,872 INFO  [MainServlet                   ] [http request: /editProject/IBN                    ] [georgiosd@127.0.0.1] Finish processing request /editProject/IBN in 47ms
-test test test
-adssaddasasdadsasdasd
+32432234234
