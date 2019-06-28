8043cf350a75b879663ad5c8d2d401cbfa094589
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-06-28 15:53:41 +0300

    fix #JT-3 Fixed Critical

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
 24 Apr 2013 15:25:33,906 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Start processing request /projects
 32432234234
+qweeqwqeweqweqw
