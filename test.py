d66311d51fb21be73951695ac0d49bc6ce6b4555
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-07-05 17:42:07 +0300

    #JT-3 Fixed comment xxx

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,090 DEBUG [BaseApplication               ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Created bean for session variables
 24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
 24 Apr 2013 15:25:33,906 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Start processing request /projects
-qewweeqwqew
+qewweeqwqewadsdas
+asddsadasads
