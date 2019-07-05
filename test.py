c6a07e00dfc896cfc1ac9307d9d6ba1705c985f6
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-07-05 18:59:28 +0300

    #TJ-1 Fixed Major

--- test.py
+++ test.py
@@ -5,4 +5,5 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
 24 Apr 2013 15:25:33,906 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Start processing request /projects
 qewweeqwqewadsdas
-asddsadasads
+asddsadasadsasdasdads
+asdadsadsads
