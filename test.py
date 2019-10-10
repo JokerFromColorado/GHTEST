62d817304a6ff46b7ab105b43765666a253ad7f8
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2019-10-10 17:27:50 +0300

    message #TP-1 critical fixed

--- test.py
+++ test.py
@@ -3,8 +3,6 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,090 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Start processing request /issue/IBN-2
 24 Apr 2013 15:25:17,090 DEBUG [BaseApplication               ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Created bean for session variables
 24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
-24 Apr 2013 15:25:33,906 INFO  [MainServlet                   ] [http request: /projects                           ] [georgiosd@127.0.0.1] Start processing request /projects
-qewweeqwqewadsdas
-asddsadasadsasdasdads
-asdadsadsadsqewqewewqqweqweqwe
-wqeewqeqwекнкекеннкеddfsdfssdf
+qweqwe
+wqeewqeqwекнwqeqweqweqew
+rrweewr
