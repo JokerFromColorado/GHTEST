5c1890814d30064866ce3186b6c3b01eddf4e315
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2020-01-14 14:39:57 +0300

    message #GHT-7 Fixed Critical

--- test.py
+++ test.py
@@ -3,4 +3,5 @@ asdasadsadsadssfd
 24 Apr 2013 15:25:17,090 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Start processing request /issue/IBN-2
 werewrewrewr
 werrwerewйцууйц
-йцуйцйцййцуйцуйцуwqqweqw
+йцуйцйцййцуйцуйцуwqqweqwqweqwe
+eqwqeqwqqeqeeqw
