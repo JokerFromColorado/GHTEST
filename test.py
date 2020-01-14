bd3c2cbcf4d06179fc1a140bb0583c2e1d5c263b
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2020-01-14 14:28:17 +0300

    message #GHT-3 Fixed Critical

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
 asdasadsadsadssfd
 
 24 Apr 2013 15:25:17,090 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Start processing request /issue/IBN-2
-24 Apr 2013 15:25:17,090 DEBUG [BaseApplication               ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Created bean for session variables
-24 Apr 2013 15:25:17,121 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Finish processing request /issue/IBN-2 in 31ms
-qweqwe
-wqeewqeqwекнwqeqweqweqew
-rrweewrwqeqwqweqwe
-wqewqewqweqewqewqweqewqweqwe
-ewqeqwewqeqw
-
-weqqewewqqwe
-qweewqqweqweqweweqqew
-weqwe
+werewrewrewr
+werrwerew
