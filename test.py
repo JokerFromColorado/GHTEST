d11992d040970d4a0949ea281237641812004499
Mikhail.Kapusta <Mikhail.Kapusta@jetbrains.com>
2020-03-19 21:31:56 +0300

    test #JI-3 fixed Critical

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
-asdasadsadsadssfd
-
-24 Apr 2013 15:25:17,090 INFO  [MainServlet                   ] [http request: /issue/IBN-2                        ] [internal@127.0.0.1] Start processing request /issue/IBN-2
-werewrewrewr
-werrwerewйцууйц
-йцуйцйцййцуйцуйцуwqqweqwqweqwe
-eqwqeqwqqeqeeqwqweweq
-wqe
+asdadsads
 ewqqewqeweqwqewqwe
 qweweqeqwewqasdsadasd
 asdsadadsasdasdasdasdasdads
 dsaasdadsasdads
-asdadssad
+asdadssadasddasads
